069b4316b7a99d4f8ebb2c354f9c6e5aee69f937
YuriChernigin <juriichernigin@gmail.com>
2016-02-03 23:11:38 +0300

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, :price, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|png|jpg)\Z}i,
+		message: 'URL must be image in format: gif, jpg or png.'
+	}
+	
 end
